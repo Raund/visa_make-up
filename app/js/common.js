e9d6b6a3e283c7ce19250a6b6f9f6a2654ef875a
Andrii Melnichuk <andrewlutsk88@gmail.com>
2017-01-07 16:45:13 +0200

    make interactive map

--- app/js/common.js
+++ app/js/common.js
@@ -7,4 +7,17 @@ $(function(){
         }, 800);
         return false;
     });
+    //make interactive map
+    $("map area").hover(function () {
+        console.info('hover', this);
+        var img = $(this).attr('data-hover-img');
+        //console.info('hover img', img);
+        $(img).addClass('active');
+    },
+    function () {
+        console.info('unhover', this);
+        var img = $(this).attr('data-hover-img');
+        //console.info('unhover img', img);
+        $(img).removeClass('active');
+    });
 });
